1b72acec7fd8f4aade8245ba8be99b8560bbda6a
Kuzivakwashe Muvezwa <muvezwakuzivakwashe@yahoo.com>
2022-04-10 16:15:46 +0200

    added logo

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -10,14 +10,14 @@ const IntroSection = () => {
                     <MDBRow>
                         <MDBCol className='text-center text-md-left mt-xl-5 mb-5'>
                             <MDBAnimation type='fadeInRight' delay='.3s'>
-                                <img className="rounded" src="./assets/image/logo_square.png" height="450px"/>
+                                <img className="rounded" src="./assets/image/endaoment_banner.png" height="350px"/>
                             </MDBAnimation>
                         </MDBCol>
 
                         <MDBCol className='text-center'>
                             <MDBAnimation type='fadeInLeft' delay='.3s'>
-                                <h1 style={{color: "#000000", paddingTop:"4em"}} className='h1-responsive font-weight-bold mt-sm-5'>
-                                A decentralized add referral system and ad revenue system for content creators.
+                                <h1 style={{color: "#000000", paddingTop:"2em"}} className='h1-responsive font-weight-bold mt-sm-5'>
+                                We help DAOs manage budget allocation for proposed projects. 
                                 </h1>
                             </MDBAnimation>
                         </MDBCol>
